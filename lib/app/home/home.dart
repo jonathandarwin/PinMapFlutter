e9238a44c217504d880317b2068bea171d618abc
jonathandarwin <jonathanjojo38@yahoo.com>
2019-08-27 15:29:41 +0700

    map done

--- lib/app/home/home.dart
+++ lib/app/home/home.dart
@@ -1,43 +1,123 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:pin_map/app/home/home_provider.dart';
+import 'package:pin_map/state/event_state.dart';
 import 'package:provider/provider.dart';
 
 class HomeLayout extends StatelessWidget{
   @override
   Widget build(BuildContext context) {    
     return ChangeNotifierProvider(
-      builder: (_) => HomeProvider(),
+      builder: (context) => HomeProvider(),
       child: SafeArea(
         child: Scaffold(
-          
+          body: LoadData(),
         ),
       ),
     );
   }
 }
 
+class LoadData extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {    
+    HomeProvider _provider = Provider.of<HomeProvider>(context, listen: false);
+
+    return FutureBuilder(
+      future: _provider.requestListPlace(),
+      initialData: null,
+      builder: (context, snapshot){
+        if(snapshot.connectionState == ConnectionState.done){
+          switch(snapshot.data){
+            case EventState.SUCCESS:            
+              return RootHome();
+              break;
+            case EventState.ERROR:
+              return ErrorMessage();
+              break;
+          }
+        }
+        return Loader();
+      },
+    );
+  }
+
+}
+
+class Loader extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {    
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
+
+class ErrorMessage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {    
+    return Center(
+      child: Text(
+        'Error. Please try again',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 15.0
+        ),      
+      ),
+    );
+  }
+}
+
+class RootHome extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {    
+    return Stack(
+      children: <Widget>[
+        Map(),
+        IconSearch()
+      ],
+    );
+  }
+
+}
+
 class Map extends StatelessWidget{
   @override
   Widget build(BuildContext context) {    
-    final Completer<GoogleMapController> _controller = Completer();
-    
+    final Completer<GoogleMapController> _controller = Completer();        
     return Consumer<HomeProvider>(
       builder: (context, provider, _) => GoogleMap(
-        markers: provider.marker,
+        markers: provider.listMarker,
         onCameraMove: provider.cameraMove,
         onMapCreated: (GoogleMapController controller){
           _controller.complete(controller);
         },
         mapType: MapType.normal,
         initialCameraPosition: CameraPosition(
-          target: provider.center,
+          target: provider.focus,
           zoom: 15
         ),
       ),
     );
   }
+}
 
+class IconSearch extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {    
+    HomeProvider _provider = Provider.of<HomeProvider>(context, listen: false);
+    return Align(
+      alignment: Alignment.bottomRight,      
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: FloatingActionButton(
+          onPressed: (){},
+          child: Icon(
+            Icons.search
+          ),
+        ),
+      ),
+    );
+  }
 }
--- lib/app/home/home_provider.dart
+++ lib/app/home/home_provider.dart
@@ -11,19 +11,55 @@ class HomeProvider extends BaseProvider{
 
   // Attribute
   Set<Marker> _listMarker = Set<Marker>();
+  LatLng _lastPosition;
+  LatLng _focus;
 
   // Getter
   Set<Marker> get listMarker => this._listMarker;
+  LatLng get focus => this._focus;
 
   // Setter
-  set listMarker(Set<Marker> listMarker){
-    this._listMarker = listMarker;
-  }
+  set listMarker(Set<Marker> listMarker) => this._listMarker = listMarker;      
+  set focus(LatLng focus) => this._focus = focus;
 
   Future<int> requestListPlace() async {
-    List<Place> listPlace = await _placeRepository.requestListPlace();
-    LatLng position = await LocationUtil.getCurrentPosition();    
-    return EventState.SUCCESS;
+    try{
+      List<Place> listPlace = await _placeRepository.requestListPlace();
+      Place currentPlace = await getCurrentLocation();
+      listPlace.add(currentPlace);
+
+      listMarker.clear();
+      for(Place place in listPlace){
+        LatLng latLng = LatLng(place.lat, place.lang);
+        listMarker.add(
+          Marker(
+            markerId: MarkerId(place.id.toString()),          
+            position: latLng,
+            infoWindow: InfoWindow(
+              title: place.description
+            ),
+            icon: BitmapDescriptor.defaultMarker
+          )
+        );
+      }
+      
+      focus = LatLng(currentPlace.lat, currentPlace.lang);
+      return EventState.SUCCESS;
+    }
+    on Exception{
+      print('<ERR> Error while loading data.');
+    } 
+    return EventState.ERROR;   
+  }
+
+  void cameraMove(CameraPosition cameraPosition){
+    _lastPosition = cameraPosition.target;
+  }
+
+  Future<Place> getCurrentLocation() async {
+    LatLng position = await LocationUtil.getCurrentPosition();
+    Place place = Place.seData(0, "Your Location", position.latitude, position.longitude);
+    return place;
   }
 
 
